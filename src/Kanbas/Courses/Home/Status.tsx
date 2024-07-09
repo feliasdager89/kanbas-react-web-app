d10972faec80034db50cb19184b3d308ba0f1955
feliasdager <feliasdager@gmail.com>
2024-07-08 23:39:46 -0400

    first commit

---
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -0,0 +1,27 @@
+export default function CourseStatus() {
+    return (
+      <div id="wd-course-status">
+        <h2>Course Status</h2>
+        <button>Unpublish</button> 
+        <button>Publish</button>
+        {/* Complete on your own */}
+        <br></br>
+        <br></br>
+        <button>View Course Notifications</button>
+        <br></br>
+        <button>Import from Commons</button>
+        <br></br>
+        <button>Course Home Page</button>
+        <br></br>
+        <button>View Course Stream</button>
+        <br></br>
+        <button>View Course Notifications</button>
+        <br></br>
+        <button>New Anouncement</button>
+        <br></br>
+        <button>New Analytics</button>
+      </div>
+  );
+}
+  
+  
